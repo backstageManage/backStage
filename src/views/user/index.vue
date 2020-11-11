c69465a5fb503fa5baa60432501e95e7c5a98dda
PhilomenaRita <1796446718@qq.com>
2020-11-11 16:54:34 +0800

    -commit
    -用户管理

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,10 +1,45 @@
 <template>
   <div>
-    User Manage
+    <el-row class="nav">
+      <div class="input-suffix">
+        输入检索：
+        <el-input size="medium" placeholder="请输入用户ID/账号" v-model="input1"> </el-input>
+      </div>
+      <div class="input-suffix">
+        收货人：
+        <el-input size="medium" placeholder="请输入用户昵称" v-model="input2"> </el-input>
+      </div>
+      <el-button type="primary">搜索</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      input1: "",
+      input2: ""
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.nav{
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  .input-suffix {
+    box-sizing: border-box;
+    width: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-right: 30px;
+  }
+  .el-input{
+    width: 70%;
+  }
+}
+</style>
